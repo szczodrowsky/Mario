732f0aba7c1c437400fc7fd4d1e49e4c7b6a728b
Adam <adam.szczodro@gmail.com>
2022-12-28 15:53:11 +0100

    półpełneanimacje

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -23,95 +24,45 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Mario;
 import com.mygdx.game.sceenes.Hud;
 import com.mygdx.game.Sprites.CMario;
+import com.mygdx.game.tools.B2WorldCreator;
 
 public class PlayScreen implements Screen {
-    private Hud hud;
+    //gra
     private Mario game;
+    private TextureAtlas atlas;
+    //zmienne kamery
     private OrthographicCamera gamecam;
     private Viewport gamePort;
-
+    private Hud hud;
+    //zmienne dla TiltedMapy
     private TmxMapLoader maploader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+    //zmienne Box2D
     private World world;
     private Box2DDebugRenderer b2dr;
+    //zmienna dla klasy Cmario
     private CMario player;
 
 
-    public PlayScreen(Mario game){
+    public PlayScreen(Mario game) {
+        atlas = new TextureAtlas("Mario_and_Enemies.pack");
         this.game = game;
         gamecam = new OrthographicCamera();
-        gamePort = new FitViewport(Mario.v_width/Mario.PPM, Mario.v_high/Mario.PPM,gamecam);
+        gamePort = new FitViewport(Mario.v_width / Mario.PPM, Mario.v_high / Mario.PPM, gamecam);
         gamePort.apply();
         hud = new Hud(game.batch);
         maploader = new TmxMapLoader();
         map = maploader.load("1-1.tmx");
         gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
-        world = new World(new Vector2(0,-10),true);
-        player = new CMario(world); // initialization of  Mario class object
+        world = new World(new Vector2(0, -10), true);
+        player = new CMario(world,this);
         b2dr = new Box2DDebugRenderer();
-        renderer = new OrthogonalTiledMapRenderer(map,1/Mario.PPM);
-
-
-
-        BodyDef bdf = new BodyDef();
-        PolygonShape shape = new PolygonShape();
-        FixtureDef fdef = new FixtureDef();
-        Body body;
-
-        //to jest podłoga
-        for(MapObject object: map.getLayers().get(1).getObjects().getByType(RectangleMapObject.class)){
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdf.type = BodyDef.BodyType.StaticBody;
-            bdf.position.set((rect.getX()+rect.getWidth()/2)/Mario.PPM, (rect.getY() +rect.getHeight()/2)/Mario.PPM);
-
-            body = world.createBody(bdf);
-
-            shape.setAsBox((rect.getWidth()/2)/Mario.PPM ,(rect.getHeight()/2)/Mario.PPM);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-        //to są rury
-        for(MapObject object: map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdf.type = BodyDef.BodyType.StaticBody;
-            bdf.position.set((rect.getX()+rect.getWidth()/2)/Mario.PPM, (rect.getY() +rect.getHeight()/2)/Mario.PPM);
-
-            body = world.createBody(bdf);
-
-            shape.setAsBox((rect.getWidth()/2)/Mario.PPM ,(rect.getHeight()/2)/Mario.PPM);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-        // to beda monety/melko
-        for(MapObject object: map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdf.type = BodyDef.BodyType.StaticBody;
-            bdf.position.set((rect.getX()+rect.getWidth()/2)/Mario.PPM, (rect.getY() +rect.getHeight()/2)/Mario.PPM);
-
-            body = world.createBody(bdf);
-
-            shape.setAsBox((rect.getWidth()/2)/Mario.PPM ,(rect.getHeight()/2)/Mario.PPM);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-            // to sa boxy
-        for(MapObject object: map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdf.type = BodyDef.BodyType.StaticBody;
-            bdf.position.set((rect.getX()+rect.getWidth()/2)/Mario.PPM, (rect.getY() +rect.getHeight()/2)/Mario.PPM);
-
-            body = world.createBody(bdf);
-
-            shape.setAsBox((rect.getWidth()/2)/Mario.PPM ,(rect.getHeight()/2)/Mario.PPM);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-
+        renderer = new OrthogonalTiledMapRenderer(map, 1 / Mario.PPM);
+        new B2WorldCreator(world,map);
+    }
+    public TextureAtlas getAtlas() {
+        return atlas;
     }
     public void handleInput(float dt){
         if (Gdx.input.isKeyJustPressed(Input.Keys.UP))
@@ -125,6 +76,7 @@ public class PlayScreen implements Screen {
     public void update (float dt){
         handleInput(dt);
         world.step(1/60f,6,2);
+        player.update(dt);
         gamecam.position.x = player.b2body.getPosition().x;
         gamecam.update();
         renderer.setView(gamecam );
@@ -139,10 +91,19 @@ public class PlayScreen implements Screen {
     @Override
     public void render(float delta) {
         update(delta);
+        //czyszczenie ekranu na czarno
         Gdx.gl.glClearColor( 0, 0, 0, 1 );
         Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+        //render naszej mapy
         renderer.render();
+        //render Box2D
         b2dr.render(world,gamecam.combined);
+
+        game.batch.setProjectionMatrix(gamecam.combined);
+        game.batch.begin();
+        player.draw(game.batch);
+        game.batch.end();
+
       game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
       hud.stage.draw();
 
@@ -170,6 +131,11 @@ public class PlayScreen implements Screen {
 
     @Override
     public void dispose() {
+        map.dispose();
+        renderer.dispose();
+        world.dispose();
+        b2dr.dispose();
+        hud.dispose();
 
     }
 }
--- core/src/com/mygdx/game/sceenes/Hud.java
+++ core/src/com/mygdx/game/sceenes/Hud.java
@@ -7,11 +7,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Mario;
 
-public class Hud {
+public class Hud implements Disposable {
     public Stage stage;
     private Viewport viewport;
     private Integer worldTimer;
@@ -53,5 +54,10 @@ public class Hud {
 
         stage.addActor(table);
 
+    }
+
+    @Override
+    public void dispose() {
+    stage.dispose();
     }
 }
